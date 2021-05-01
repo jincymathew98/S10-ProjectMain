fa4da69a8cd24eb2023e783abaafe5b5fe422ac4
jincymathew98 <jincymathew@mca.ajce.in>
2021-05-01 11:20:20 +0530

    Update myorders.php

--- Dairy/dfsms/myorders.php
+++ Dairy/dfsms/myorders.php
@@ -1,16 +1,123 @@
-<body class="cnt-home">
+<?php
+session_start();
+//error_reporting(0);
+include('includes/config.php');
+// if (strlen($_SESSION['aid']==0)) {
+//   header('location:logout.php');
+//   } else{ ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+    <title>Dashboard</title>
+    <link href="vendors/vectormap/jquery-jvectormap-2.0.3.css" rel="stylesheet" type="text/css" />
+    <link href="vendors/jquery-toggles/css/toggles.css" rel="stylesheet" type="text/css">
+    <link href="vendors/jquery-toggles/css/themes/toggles-light.css" rel="stylesheet" type="text/css">
+    <link href="vendors/jquery-toast-plugin/dist/jquery.toast.min.css" rel="stylesheet" type="text/css">
+    <link href="dist/css/style.css" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+    
+	
+	<!-- HK Wrapper -->
+	<div class="hk-wrapper hk-vertical-nav">
+
+<?php include_once('includes/navbar.php');
+include_once('includes/sidebar.php');
+?>
+        <div id="hk_nav_backdrop" class="hk-nav-backdrop"></div>
+        <!-- /Vertical Nav -->
+        <!-- Main Content -->
+        <div class="hk-pg-wrapper">
+			<!-- Container -->
+            <div class="container-fluid mt-xl-50 mt-sm-30 mt-15">
+                <!-- Row -->
+                <div class="row">
+                    <div class="col-xl-12">
+                        <div class="hk-row">
+
+
+							
+
+
+
+
+
+<?php 
+
+error_reporting(0);
+include('includes/config.php');
+// if(strlen($_SESSION['login'])==0)
+//     {   
+// header('location:login.php');
+// }
+// else{
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<!-- Meta -->
+		<meta charset="utf-8">
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+		<meta name="description" content="">
+		<meta name="author" content="">
+	    <meta name="keywords" content="MediaCenter, Template, eCommerce">
+	    <meta name="robots" content="all">
+
+	    <title>Order History</title>
+	    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+	    <link rel="stylesheet" href="assets/css/main.css">
+	    <link rel="stylesheet" href="assets/css/green.css">
+	    <link rel="stylesheet" href="assets/css/owl.carousel.css">
+		<link rel="stylesheet" href="assets/css/owl.transitions.css">
+		<!--<link rel="stylesheet" href="assets/css/owl.theme.css">-->
+		<link href="assets/css/lightbox.css" rel="stylesheet">
+		<link rel="stylesheet" href="assets/css/animate.min.css">
+		<link rel="stylesheet" href="assets/css/rateit.css">
+		<link rel="stylesheet" href="assets/css/bootstrap-select.min.css">
+
+		<!-- Demo Purpose Only. Should be removed in production -->
+		<link rel="stylesheet" href="assets/css/config.css">
+
+		<link href="assets/css/green.css" rel="alternate stylesheet" title="Green color">
+		<link href="assets/css/blue.css" rel="alternate stylesheet" title="Blue color">
+		<link href="assets/css/red.css" rel="alternate stylesheet" title="Red color">
+		<link href="assets/css/orange.css" rel="alternate stylesheet" title="Orange color">
+		<link href="assets/css/dark-green.css" rel="alternate stylesheet" title="Darkgreen color">
+		<link rel="stylesheet" href="assets/css/font-awesome.min.css">
+		<link href='http://fonts.googleapis.com/css?family=Roboto:300,400,500,700' rel='stylesheet' type='text/css'>
+		<link rel="shortcut icon" href="assets/images/favicon.ico">
+
+<script language="javascript" type="text/javascript">
+var popUpWin=0;
+function popUpWindow(URLStr, left, top, width, height)
+{
+ if(popUpWin)
+{
+if(!popUpWin.closed) popUpWin.close();
+}
+popUpWin = open(URLStr,'popUpWin', 'toolbar=no,location=no,directories=no,status=no,menubar=no,scrollbars=yes,resizable=no,copyhistory=yes,width='+600+',height='+600+',left='+left+', top='+top+',screenX='+left+',screenY='+top+'');
+}
+
+</script>
+
+	</head>
+    <body class="cnt-home">
 	
 		
 	
-		<!-- ============================================== HEADER ============================================== -->
-
-<!-- ============================================== HEADER : END ============================================== -->
 <div class="breadcrumb">
 	<div class="container">
 		<div class="breadcrumb-inner">
 			<ul class="list-inline list-unstyled">
-				<li><a href="#">Home</a></li>
-				<li class='active'>Shopping Cart</li>
+				<li><a href="dashboard.php">Home / My orders</li></a>
+				
 			</ul>
 		</div><!-- /.breadcrumb-inner -->
 	</div><!-- /.container -->
@@ -24,24 +131,58 @@
 	<div class="table-responsive">
 <form name="cart" method="post">	
 
-
-<table class="table table-bordered">
+		<table class="table table-bordered">
 			<thead>
+				
+			
+				
+				
+				
+				
 				<tr>
 					<th class="cart-romove item">#</th>
-					<th class="cart-description item">Image</th>
-					<th class="cart-product-name item">Product Name</th>
+		
+					<th class="cart-product-name item"> Name</th>
 			
-					<th class="cart-qty item">Quantity</th>
-					<th class="cart-sub-total item">Price Per unit</th>
-					<th class="cart-sub-total item">Shipping Charge</th>
-					<th class="cart-total item">Grandtotal</th>
+					<th class="cart-qty item">Email</th>
+					<th class="cart-sub-total item">Phone Number</th>
+					<th class="cart-sub-total item">Address</th>
+		
 					<th class="cart-total item">Payment Method</th>
-					<th class="cart-description item">Order Date</th>
+					<th class="cart-description item">Products</th>
 					<th class="cart-total last-item">Action</th>
 				</tr>
 			</thead><!-- /thead -->
-            	
+			
+			<tbody>
+
+			<?php 
+								
+									$sql = "select * from orders";
+									$Resulta = mysqli_query($con,$sql);
+                                    $cnt=1;
+									while($Rows = mysqli_fetch_array($Resulta)):; 
+								?>
+								<tr style="color: black">
+						
+									<td><?php echo $cnt; ?></td>
+									<td><?php echo $Rows["name"]; ?></td>
+									<td><?php echo $Rows["email"]; ?></td>
+									<td><?php echo $Rows["phone"]; ?></td>
+									<td><?php echo $Rows["address"]; ?></td>
+									<td><?php echo $Rows["payment_mode"]; ?></td>
+									<td><?php echo $Rows["products"]; ?></td>
+									
+									
+					
+									<td><a href="javascript:void(0);" onClick="popUpWindow('track-order.php?oid=<?php echo htmlentities($row['orderid']);?>');" title="Track order"> </td>
+
+ </tr>
+ <?php 
+                                $cnt++;
+                                endwhile; ?>
+								
+				
 			</tbody><!-- /tbody -->
 		</table><!-- /table -->
 		
@@ -53,3 +194,83 @@
 		</form>
 		</div><!-- /.container -->
 </div><!-- /.body-content -->
+
+
+	<script src="assets/js/jquery-1.11.1.min.js"></script>
+	
+	<script src="assets/js/bootstrap.min.js"></script>
+	
+	<script src="assets/js/bootstrap-hover-dropdown.min.js"></script>
+	<script src="assets/js/owl.carousel.min.js"></script>
+	
+	<script src="assets/js/echo.min.js"></script>
+	<script src="assets/js/jquery.easing-1.3.min.js"></script>
+	<script src="assets/js/bootstrap-slider.min.js"></script>
+    <script src="assets/js/jquery.rateit.min.js"></script>
+    <script type="text/javascript" src="assets/js/lightbox.min.js"></script>
+    <script src="assets/js/bootstrap-select.min.js"></script>
+    <script src="assets/js/wow.min.js"></script>
+	<script src="assets/js/scripts.js"></script>
+
+	<!-- For demo purposes – can be removed on production -->
+	
+	<script src="switchstylesheet/switchstylesheet.js"></script>
+	
+	<script>
+		$(document).ready(function(){ 
+			$(".changecolor").switchstylesheet( { seperator:"color"} );
+			$('.show-theme-options').click(function(){
+				$(this).parent().toggleClass('open');
+				return false;
+			});
+		});
+
+		$(window).bind("load", function() {
+		   $('.show-theme-options').delay(2000).trigger('click');
+		});
+	</script>
+	<!-- For demo purposes – can be removed on production : End -->
+</body>
+</html>
+
+
+
+
+
+
+</div>
+					
+            </div>
+            <!-- /Container -->
+			
+         
+        </div>
+        <!-- /Main Content -->
+
+    </div>
+    <!-- /HK Wrapper -->
+
+    <!-- jQuery -->
+    <script src="vendors/jquery/dist/jquery.min.js"></script>
+    <script src="vendors/popper.js/dist/umd/popper.min.js"></script>
+    <script src="vendors/bootstrap/dist/js/bootstrap.min.js"></script>
+    <script src="dist/js/jquery.slimscroll.js"></script>
+    <script src="dist/js/dropdown-bootstrap-extended.js"></script>
+    <script src="dist/js/feather.min.js"></script>
+    <script src="vendors/jquery-toggles/toggles.min.js"></script>
+    <script src="dist/js/toggle-data.js"></script>
+	<script src="vendors/waypoints/lib/jquery.waypoints.min.js"></script>
+	<script src="vendors/jquery.counterup/jquery.counterup.min.js"></script>
+    <script src="vendors/jquery.sparkline/dist/jquery.sparkline.min.js"></script>
+    <script src="vendors/vectormap/jquery-jvectormap-2.0.3.min.js"></script>
+    <script src="vendors/vectormap/jquery-jvectormap-world-mill-en.js"></script>
+	<script src="dist/js/vectormap-data.js"></script>
+    <script src="vendors/owl.carousel/dist/owl.carousel.min.js"></script>
+    <script src="vendors/jquery-toast-plugin/dist/jquery.toast.min.js"></script>
+    <script src="vendors/apexcharts/dist/apexcharts.min.js"></script>
+	<script src="dist/js/irregular-data-series.js"></script>
+    <script src="dist/js/init.js"></script>
+	
+</body>
+
+</html>
